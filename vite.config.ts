81dea08604b11e824b24b53a7ad3b04d78bd29c3
ankushKun <ankush4singh@gmail.com>
2023-11-12 18:00:07 +0530

    chunking build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   build: {
-    chunkSizeWarningLimit: 500
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+          return undefined;
+        },
+      }
+    }
   }
 })
